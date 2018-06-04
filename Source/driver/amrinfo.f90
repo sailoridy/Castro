8ea21324405f7cc77314c69d6a7f6183202531e8
dwillcox <eugene.willcox@gmail.com>
2018-06-04 16:10:23 -0400

    Make amr_level a CUDA managed memory variable.

--- Source/driver/amrinfo.f90
+++ Source/driver/amrinfo.f90
@@ -6,11 +6,24 @@ module amrinfo_module
   use amrex_fort_module, only : rt => amrex_real
   implicit none
 
-  integer :: amr_level = 0
+  integer, allocatable :: amr_level
   integer :: amr_iteration = 0
   integer :: amr_ncycle = 0
 
   real(rt)         :: amr_time = 0.0
   real(rt)         :: amr_dt = 0.0
+
+#ifdef CUDA
+  attributes(managed) :: amr_level
+#endif
+
+contains
   
+  subroutine amrinfo_init()
+    if (.not. allocated(amr_level)) then
+       allocate(amr_level)
+       amr_level = 0
+    endif
+  end subroutine amrinfo_init
+
 end module amrinfo_module
